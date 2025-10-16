29fbb03b67f74bef48e64fdb11d63ece78733e22
Andriy <andriyshobotenko@gmail.com>
2025-10-16 12:09:35 +0300

    Add files via upload

---
+++ CW_lesson7.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+lower_red1 = np.array([0, 100, 100])
+upper_red1 = np.array([10, 255, 255])
+
+lower_red2 = np.array([160, 100, 100])
+upper_red2 = np.array([180, 255, 255])
+
+points = []
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        obj = None
+        if cv2.contourArea(cnt) > 1000:
+            color = (0, 255, 0)
+            cv2.drawContours(frame, [cnt], -1, color, 2)
+            M = cv2.moments(cnt)
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+                cv2.circle(frame, (cx, cy), 2, (255, 0, 0), 2)
+        #       points.append((cx, cy))
+        #   for i in range(1, len(points)):
+        #       if points[i - 1] is None or  points[i] is None:
+        #           continue
+        #       cv2.line(frame, points[i - 1], points[i], (255, 0, 0), 2)
+            cv2.rectangle(frame, (0, 0), (frame.shape[1], frame.shape[0]), (0, 255, 0), 2)
+        else:
+            color = (0, 0, 255)
+            cv2.rectangle(frame, (0, 0), (frame.shape[1], frame.shape[0]), color, 2)
+        while color == (0, 255, 0):
+            obj = 'Detected'
+        while color == (0, 0, 255):
+            obj = 'Not Detected'
+        cv2.putText(frame, obj, (10, 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, color, 1)
+
+
+
+    cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
