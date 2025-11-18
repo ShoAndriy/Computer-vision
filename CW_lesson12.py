5da2393eadbf64ec2b7ba6eef53291dcf866f1a9
Andriy <andriyshobotenko@gmail.com>
2025-11-18 14:21:41 +0200

    Add files via upload

---
+++ CW_lesson12.py
@@ -0,0 +1,117 @@
+# import tensorflow as tf
+# from tensorflow.keras import (layers, models)
+# import numpy as np
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.python.layers.normalization import normalization
+#
+#
+# train_ds = tf.keras.preprocessing.image_dataset_from_directory("data/train", image_size = (128, 128),
+#                                                                batch_size = 32, label_mode = "categorical")
+# test_ds = tf.keras.preprocessing.image_dataset_from_directory("data/test", image_size = (128, 128),
+#                                                                batch_size = 32,label_mode = "categorical")
+#
+# normalization_layer = layers.Rescaling(1./255)
+#
+# train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# test_ds = test_ds.map(lambda x, y: (normalization_layer(x), y))
+#
+# model = models.Sequential()
+#
+# model.add(layers.Conv2D(
+#     filters = 32,
+#     kernel_size = (3, 3),
+#     activation = "relu",
+#     input_shape = (128, 128, 3)
+# ))
+# model.add(layers.MaxPooling2D((2, 2)))
+#
+# model.add(layers.Conv2D(64, (3, 3), activation = "relu"))
+# model.add(layers.MaxPooling2D((2, 2)))
+#
+# model.add(layers.Conv2D(128, (3, 3), activation = "relu"))
+# model.add(layers.MaxPooling2D((2, 2)))
+#
+# model.add(layers.Flatten())
+#
+# model.add(layers.Dense(64, activation = "relu"))
+# model.add(layers.Dense(3, activation = "softmax"))
+#
+# model.compile(
+#     optimizer = "adam",
+#     loss = "categorical_crossentropy",
+#     metrics = ["accuracy"]
+# )
+#
+# history = model.fit(train_ds, epochs=50, validation_data=test_ds)
+#
+# test_loss, test_acc = model.evaluate(test_ds)
+# print(f"{test_acc}")
+#
+# class_name = ["cars", "cats", "dogs"]
+#
+# img = image.load_img()
+#
+# image_array = image.img_to_array(img)
+# image_array = image_array/255
+# image_array = np.expand_dims(image_array, axis=0)
+# predictions = model.predict(image_array)
+# predict_index = np.argmax(predictions)
+
+import tensorflow as tf
+from tensorflow.keras import (layers, models)
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.python.layers.normalization import normalization
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory('data/train', image_size=(128, 128), batch_size=30, label_mode='categorical')
+test_ds = tf.keras.preprocessing.image_dataset_from_directory('data/test', image_size=(128, 128), batch_size=30, label_mode='categorical')
+
+#3. НОРМАЛІЗАЦІЯ ЗОБРАЖЕНЬ
+normalization_layer = layers.Rescaling(1./255)
+train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+test_ds = test_ds.map(lambda x, y: (normalization_layer(x), y))
+
+#4. ВХІДНИЙ ШАР
+model = models.Sequential()
+
+model.add(layers.Conv2D(filters = 32, kernel_size = (3, 3),
+                        activation = 'relu', input_shape = (128, 128, 1)))
+#filters = простий
+model.add(layers.MaxPooling2D(2, 2))
+
+model.add(layers.Conv2D(filters = 62, kernel_size = (3, 3),
+                        activation = 'relu', input_shape = (128, 128, 1)))
+model.add(layers.MaxPooling2D(2, 2))
+
+model.add(layers.Conv2D(filters = 128, kernel_size = (3, 3),
+                        activation = 'relu', input_shape = (128, 128, 1)))
+model.add(layers.MaxPooling2D(2, 2))
+
+model.add(layers.Flatten())
+
+#5. ВНУТРІШНІЙ ШАР
+model.add(layers.Dense(64, activation = 'relu'))
+model.add(layers.Dense(3, activation = 'softmax'))
+
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.summary()
+history = model.fit(train_ds,
+                    validation_data=test_ds,
+                    epochs=15,
+                    verbose=2)
+test_loss, test_acc = model.evaluate(test_ds)
+print(f'Якість: {test_acc}')
+
+#6. ПЕРЕВІРКА.
+class_name = ['car', 'cats', 'dogs']
+img = image.load_img('Images/', target_size = (128, 128))
+img_array = image.img_to_array(img)
+img_array = img_array / 255.0
+
+img_array = np.expand_dims(img_array, axis = 0)
+predictions = model.predict(img_array)
+
+predicted_index = np.argmax(predictions[0])
+
+print(f'Імовірність по класам: {predictions[0]}')
+print(f'Модель визначила: {class_name[predicted_index]}')
